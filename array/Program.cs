9c4245c0e0eb577f77d98efce16515e0eb0a9bb3
Lizaveta Valynets <volynets2000@mail.ru>
2023-04-28 22:40:52 +0300

    the programm is finished

--- array/Program.cs
+++ array/Program.cs
@@ -5,9 +5,36 @@
 лучше обойтись исключительно массивами. */
 
 string[] smile = {":)", "welcome", ":3", "hello", ":D", "good day", ";)"};
+int count = Counting(smile);
+string[] newSmile = CreateNewArray(smile, count);
+PrintArray(newSmile);
 
+int Counting(string[] smile)
+{
+int count = 0;
+ for (int i = 0; i < smile.Length; i++)
+{
+    if (smile[i].Length <= 3)
+        count++;
+}
+return count;
+}
 
+string[] CreateNewArray(string[] array, int count)
+{
+    string[] array2 = new string[count];
+    int j = 0;
 
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array2[j] = array[i];
+            j++;
+        }
+    }
+    return array2;
+}
 
 void PrintArray(string[] array)
 {
